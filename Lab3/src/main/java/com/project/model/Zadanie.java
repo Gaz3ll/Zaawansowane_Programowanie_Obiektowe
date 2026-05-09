5df5d702085f48f0d3830fd6b5e1c5ec31032241
Gaz3ll <lesniewskiw@outlook.com>
2026-05-09 12:28:28 +0200

    Add files via upload

---
+++ Lab3/src/main/java/com/project/model/Zadanie.java
@@ -0,0 +1,57 @@
+package com.project.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Table(name="zadanie")
+public class Zadanie {
+    
+    @Id
+    @GeneratedValue
+    @Column(name="zadanie_id")
+    private Integer zadanieId;
+
+    @NotBlank(message = "Pole nazwa nie może być puste!")
+    @Column(nullable = false, length = 50)
+    private String nazwa;
+
+    @Column(length = 1000)
+    private String opis;
+
+    private Integer kolejnosc;
+
+    @CreatedDate
+    @Column(name = "dataczas_dodania", nullable = false, updatable = false)
+    private LocalDateTime dataczasDodania;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "projekt_id")
+    private Projekt projekt;
+
+}
